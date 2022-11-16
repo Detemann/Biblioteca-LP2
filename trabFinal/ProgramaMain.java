22ba0034e964e2da620ce816a4bb8805bbec4fdc
Wanderson Gonçalves <105672201+Wandersontr01@users.noreply.github.com>
2022-11-16 15:04:22 +0000

    Modificação realizada no menu principal

--- trabFinal/ProgramaMain.java
+++ trabFinal/ProgramaMain.java
@@ -1,19 +1,22 @@
 package trabFinal;
+import java.util.*;
 
 public class ProgramaMain {
+	static Scanner entrada = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
 		//MENU DE ESCOLHA
-		System.out.println("\n=============== ALGUM NOME ===============");
+		System.out.println("\n=============== BIBLIOTECA DO(A) XXXXXXXXXX ===============");
 		int op = 0;
 		while (op != 9) {
+			//PRECISA SER REALIZADO UMA AUTENTICACAO DE USUARIO ANTES DE EXIBIR A TELA DE MENU
 			System.out.println("Informe o opcao desejada\n"
-					+ "[1] Funcao1\n"
-					+ "[2] Funcao2\n"
-					+ "[3] Funcao3\n"
-					+ "[4] Funcao4\n"
-					+ "[5] Funcao5\n"
+					+ "[1] Empréstimo de livros\n"
+					+ "[2] Devolução de Livros\n"
+					+ "[3] Cadastrar novos Livros\n"
+					+ "[4] Cadastrar novos Usuários\n" //Aluno ou Funcionário
+					+ "[5] Relatórios\n" //Livros cadastrados; Empréstimos realizados; Funcionários cadastrados; Alunos cadastrados; Imprimir multa individual;
 					+ "[6] Funcao6");
 			
 			op = entrada.nextInt();
