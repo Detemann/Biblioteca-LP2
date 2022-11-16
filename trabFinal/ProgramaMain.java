3fdf231fd18d4da8c167958b56742faba57a204d
Wanderson Gonçalves <105672201+Wandersontr01@users.noreply.github.com>
2022-11-16 14:54:52 +0000

    Menu adicionado no main

--- trabFinal/ProgramaMain.java
+++ trabFinal/ProgramaMain.java
@@ -3,7 +3,48 @@ package trabFinal;
 public class ProgramaMain {
 
 	public static void main(String[] args) {
-		System.out.println("Teste Wandebas");
+
+		//MENU DE ESCOLHA
+		System.out.println("\n=============== ALGUM NOME ===============");
+		int op = 0;
+		while (op != 9) {
+			System.out.println("Informe o opcao desejada\n"
+					+ "[1] Funcao1\n"
+					+ "[2] Funcao2\n"
+					+ "[3] Funcao3\n"
+					+ "[4] Funcao4\n"
+					+ "[5] Funcao5\n"
+					+ "[6] Funcao6");
+			
+			op = entrada.nextInt();
+
+			switch (op) {
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			case 5:
+				
+				break;
+			case 6:
+				
+				break;
+			case 9:
+				System.out.println("Fim de programa!");
+				break;
+			default:
+				System.out.println("A opcao informada é inválida!");
+				break;
+			}
+		}
 		
 	}
 
