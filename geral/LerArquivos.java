1623f412e4c29015f8db0f38c208afae29c28427
rapharias01 <107948220+rapharias01@users.noreply.github.com>
2022-11-23 14:19:03 -0300

    Criacao do "Ler Arquivos"

--- geral/LerArquivos.java
+++ geral/LerArquivos.java
@@ -2,22 +2,32 @@ package geral;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 
 public class LerArquivos {
 	public void imprimeTudo() {
-		String linha;
-
 		try {
-			BufferedReader leitor = new BufferedReader(new FileReader("csv\\ALUNOS.csv"));
-			ArrayList<String[]> Listas = new ArrayList<>();
-			while ((linha = leitor.readLine()) != null) {
-				String[] Lista = linha.split(";");
+			String[] caminhos = new String[7];
+			caminhos[0] = "csv\\FUNCIONARIOS.csv";
+			caminhos[1] = "csv\\PROFESSORES.csv";
+			caminhos[2] = "csv\\ALUNOS.csv";
+			caminhos[3] = "csv\\LIVROS.csv";
+			caminhos[4] = "csv\\PERIODICOS.csv";
+			caminhos[5] = "csv\\EMPRESTIMO.csv";
+			caminhos[6] = "csv\\ITENS.csv";
+			for (String string : caminhos) {
+				BufferedReader file = new BufferedReader(new FileReader(string));
+				String line;
+				String input = "";
+				while ((line = file.readLine()) != null) {
+					input += line + '\n';
+				}
+				file.close();
+				System.out.println(input + "\n"
+						+ "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 			}
-			leitor.close();
-			System.out.println(Listas);
 		} catch (Exception e) {
 			System.out.println("Ocorreu um erro.");
+			e.printStackTrace();
 		}
 	}
 }
