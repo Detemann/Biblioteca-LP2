295fd1de42b891b5c826883d42255b86f994b383
Detemann <110741308+Detemann@users.noreply.github.com>
2022-12-07 16:20:24 -0300

    Update 1.0.4

--- geral/FazerDevolucao.java
+++ geral/FazerDevolucao.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.OutputStreamWriter;
+import java.util.Objects;
 
 public class FazerDevolucao {
 	Auxiliares auxi = new Auxiliares();
@@ -20,7 +21,52 @@ public class FazerDevolucao {
 		} while (matricula == -1);
 		
 		String[] statusEmprestimo = auxi.buscaEmprestimo(matricula, 1);
-		if (statusEmprestimo[1].equals("0")) {
+		if (statusEmprestimo[1].equals("1")) {
+			
+			String [] infoAluno = new String[6];
+			try {
+				BufferedReader leitor = new BufferedReader(new FileReader("csv\\ALUNOS.csv"));
+				String aluno;
+				while((aluno = leitor.readLine()) != null) {
+					if (aluno.contains(String.valueOf(matricula))) {
+						infoAluno = aluno.split(";");
+						leitor.close();
+						break;
+					}
+				}
+				
+				leitor = new BufferedReader(new FileReader("csv\\ALUNOS.csv"));
+				String linhaAluno;
+				String novasLinhasAluno = "";
+				while ((linhaAluno = leitor.readLine()) != null) {
+					novasLinhasAluno += linhaAluno + "\n";
+				}
+				String alunoStr = "";
+				for (String string : infoAluno) {
+					alunoStr += ";" + string;
+				}
+				alunoStr = alunoStr.replaceFirst(";", "");
+				if (alunoStr.contains("0.0")) {
+					novasLinhasAluno = novasLinhasAluno.replace(alunoStr, alunoStr = alunoStr.replace("0.0", "20.0"));
+				}
+				leitor.close();
+				
+				OutputStreamWriter escritor = new OutputStreamWriter(new FileOutputStream("csv\\ALUNOS.csv", false), "UTF-8");
+				escritor.write(novasLinhasAluno);
+				escritor.close();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			auxi.buscarMulta(matricula);
+			System.out.println("Aluno pagou a multa? [S/N]");
+			String resultado = ProgramaMain.entrada.next();
+			if (Objects.equals(resultado, "N")) {
+				System.out.println("Aluno tem que pagar a multa para devolver o livro.");
+				ProgramaMain.menuPrincipal();
+			}
+		}
 			Integer[] codigoItem = auxi.buscaItem(Integer.parseInt(statusEmprestimo[0]));
 			String[] item = auxi.buscaArcevo(String.valueOf(codigoItem[0]), codigoItem[1]);
 			auxi.setDisponibilidade(item, codigoItem[1]);
@@ -46,6 +92,42 @@ public class FazerDevolucao {
 					if(passou==true) {
 						break;
 					}
+				}
+				
+				String [] infoAluno = new String[6];
+				try {
+					leitor = new BufferedReader(new FileReader("csv\\ALUNOS.csv"));
+					String aluno;
+					while((aluno = leitor.readLine()) != null) {
+						if (aluno.contains(String.valueOf(matricula))) {
+							infoAluno = aluno.split(";");
+							leitor.close();
+							break;
+						}
+					}
+					
+					leitor = new BufferedReader(new FileReader("csv\\ALUNOS.csv"));
+					String linhaAluno;
+					String novasLinhasAluno = "";
+					while ((linhaAluno = leitor.readLine()) != null) {
+						novasLinhasAluno += linhaAluno + "\n";
+					}
+					String alunoStr = "";
+					for (String string : infoAluno) {
+						alunoStr += ";" + string;
+					}
+					alunoStr = alunoStr.replaceFirst(";", "");
+					if (alunoStr.contains("20.0")) {
+						novasLinhasAluno = novasLinhasAluno.replace(alunoStr, alunoStr = alunoStr.replace("20.0", "0.0"));
+					}
+					leitor.close();
+					
+					OutputStreamWriter escritor = new OutputStreamWriter(new FileOutputStream("csv\\ALUNOS.csv", false), "UTF-8");
+					escritor.write(novasLinhasAluno);
+					escritor.close();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 				
 				String linhasEmprestimo;
@@ -82,10 +164,5 @@ public class FazerDevolucao {
 			}
 			
 			System.out.println("FIM\n\n");
-		} else {
-			auxi.buscarMulta(matricula);
-		}
-		
-		
 	}
 }
